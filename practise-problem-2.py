0effeb8b2d967deb6d6194bb83600319070e6ddf
Sannidhya Dasgupta <72875652+Sannidhya4396@users.noreply.github.com>
2020-10-27 16:02:03 +0530

    ADDE

---
+++ practise-problem-2.py
@@ -0,0 +1,47 @@
+# Practice Problem 2 (Easy) | Python Tutorials For Absolute Beginners In Hindi
+# The task you have to perform is “Divide the Apples.” This task consists of a total of 10 points to evaluate your performance.
+
+# Problem Statement: -
+# Harry Potter has got the “n” number of apples. Harry has some students among whom he wants to distribute the apples. These “n” number of apples is provided to harry by his friends, and he can request for few more or few less apples.
+
+# You need to print whether a number is in range mn to mx, is a divisor of “n” or not.
+
+# Input:
+
+# Take input n, mn, and mx from the user.
+
+# Output:
+# Print whether the numbers between mn and mx are divisor of “n” or not. If mn = mx, show that this is not a range, and mn is equal to mx. Show the result for that number.
+
+# Example:
+# If n is 20 and mn = 2 and mx = 5
+
+# 2 is a divisor of20
+
+# 3 is not a divisor of 20
+
+# …
+
+# 5 is a divisor of 20
+try:
+    nTerm = int(
+        input("Please enter the number whose divisor you wish to find out:\n"))
+
+    mnTerm = int(input(
+        "Please enter the first number in the range from where you want to start counting the divisor"))
+    mxTerm = int(input(
+        "Please enter the second number in the range from where you want to stop counting the divisor"))
+    for i in range(mnTerm, mxTerm+1):
+        divided = nTerm % i
+        if mnTerm < mxTerm:
+            if divided == 0:
+                # print(nTerm / i)
+                print(f"The number {i} is a divisor of {nTerm}")
+
+            elif nTerm % i != 0:
+                print(f"The number {i} is not a divisor of {nTerm}")
+
+
+except ValueError:
+    print(
+        "Please don't enter any kind of punctuations ()!@#$%^&*}{:><>?][;l.,./ or alphabets ")
---
+++ practise-problem-3.py
@@ -0,0 +1,46 @@
+# Python Practice 3 | Python Tutorials For Absolute Beginners In Hindi
+# The task you have to perform is “Foods and Calories.” This task consists of a total of 15 points to evaluate your performance.
+
+# Problem Statement:-
+# You visited a restaurant called CodeWithHarry, and the food items in that restaurant are sorted, based on their amount of calories. You have to reserve this list of food items containing calories.
+
+# You have to use the following three methods to reserve a list:
+
+# Inbuild method of python
+# List name [::-1] slicing trick
+# Swapping the first element with the last one and second element with second last one and so on like,
+# [6 7 8 34 5] -> [5 34 8 7 6]
+
+# Input:
+# Take a list as an input from the user
+
+# [5, 4, 1]
+
+# Output:
+# [1, 4, 5]
+
+# [1, 4, 5]
+
+# [1, 4, 5]
+
+
+# All three methods give the same results!
+
+# The program starts from here!
+
+ListIn = int(input("Please enter the size of the list:\n"))
+
+ls = []
+
+for i in range(ListIn):
+    ls.append(int(input("Please enter the list element")))
+
+print(f"Your list is:")
+for i in ls:
+    print(i)
+
+print("We have reversed the list for you")
+
+ls = ls[::-1]
+for i in ls:
+    print(i)
